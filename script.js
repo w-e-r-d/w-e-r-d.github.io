4c1fdd236e31c7055bf131025bf21b4923941e41
w-e-r-d <drew@lefebvre.us>
2025-08-29 21:52:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-document.getElementById('hello')?.addEventListener('click', () => {
-  alert('button clicked');
-});
+const tilt = (Math.random() * 3 - 1.5).toFixed(2) + 'deg';
+document.documentElement.style.setProperty('--active-tilt', tilt);
